a005676f018a13f1a18370f3c0a3447643171f82
Kaushal Jayapragash <kaushaljayapragash@MacBook-Air-68.local>
2025-05-08 16:09:02 +0800

    Temp rename MovieForm.jsx to temp.jsx to fix case conflict

---
+++ movies-project/frontend/src/components/temp.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+
+function MovieForm() {
+  return (
+    <div>MovieForm</div>
+  )
+}
+
+export default MovieForm
