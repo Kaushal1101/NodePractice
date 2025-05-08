27683b1095200fba3f135b5a0f09970ff5b87335
Kaushal Jayapragash <kaushaljayapragash@MacBook-Air-68.local>
2025-05-08 17:25:43 +0800

    Correct casing to MovieForm.jsx

--- movies-project/frontend/src/components/MovieForm.jsx
+++ movies-project/frontend/src/components/MovieForm.jsx
@@ -1,10 +1,42 @@
 import {useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-
+import {useDispatch} from 'react-redux'
+import createMovie from '../features/movies/movieSlice'
 
 function MovieForm() {
-  return (
-    <div>MovieForm</div>
+    const [text, setText] = useState('')
+
+    const dispatch = useDispatch()
+    
+    const onSubmit = (e) => {
+        console.log('C')
+        e.preventDefault()
+
+        console.log('D')
+        dispatch(createMovie({ text }))
+        console.log('E')
+        setText('')
+    } 
+
+    return (
+    <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='text'>Movie</label>
+            <input
+                type = 'text'
+                name = 'text'
+                id = 'text'
+                value = {text}
+                onChange = {(e) => setText(e.target.value)}
+             />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block" type='submit'>
+                Add Movie
+            </button>
+          </div>
+        </form>
+    </section>
   )
 }
 
